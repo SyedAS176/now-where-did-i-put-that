cc6c7aebf2c1e964404cfbae867e42d73e25a430
Redacted <146182187+SyedAS176@users.noreply.github.com>
2025-09-24 14:01:57 -0500

    Added the two requested functions. Will add their body later.

--- src/where.cpp
+++ src/where.cpp
@@ -1,5 +1,15 @@
 #include <string>
 
+using namespace std;
+
 std::string hello() {
    return "Hello World!";
+}
+
+int findCharacter() { //Finds character in a string
+
+}
+
+int findSubstring() { //Finds substring in a string
+
 }
