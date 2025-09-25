8306aa3fd0b5bc406969f48118c72d4918895081
Redacted <146182187+SyedAS176@users.noreply.github.com>
2025-09-25 15:05:54 -0500

    Added menu for the user to input strings

--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <string>
+
+  using namespace std;
 
 #include "src/where.hpp"
 
 int main() {
-  // Your driver program goes here
+  bool run = true; //Controls while loop
+  string inputString; //Store user's input string
+  int choice = -1; //Start with invalid choice
+  cout << "What string would you like to use as your input?" << endl;
+  getline(cin, inputString); //Reading user input
+  cin.ignore(); //Flush the buffer to cin can prompt user again
+
+  //Creating a menu here
+  while (run) {
+    cout << "Awesome! Would you like to search for a character or substring? \n"
+    << "1. Character \n"
+    << "2. Substring \n"
+    << "3. Exit \n"
+    << "Enter your choice: "
+    << endl;
+
+    cin >> choice; //Store user choice
+
+    if (cin.fail()) {
+      cin.clear(); //Clear the error message
+      cin.ignore(); //Flush the buffer
+
+      cout << "Invalid input. Please try again."
+      << "1. Character \n"
+      << "2. Substring \n"
+      << "3. Exit \n"
+      << "Enter your choice: "<< endl;
+    } else if (choice == 0) {
+      cout << "Bye bye!" << endl;
+      run = false; //Kills the while-loop
+    } else if (choice == 1) {
+      cout << "You chose character search.\n";
+      //-------> YOU NEED TO ADD YOUR CHARACTER FUNCTION CALL HERE!!!!! <-------
+    } else if (choice == 2) {
+      cout << "You chose substring search.\n";
+      //-------> YOU NEED TO ADD YOUR SUBSTRING FUNCTION CALL HERE!!!!! <-------
+    } else {
+      cout << "Invalid choice. Please try again." << endl;
+    }
+  }
+  return 0;
 }
--- src/where.cpp
+++ src/where.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 
 using namespace std;
@@ -6,10 +7,16 @@ std::string hello() {
    return "Hello World!";
 }
 
-int findCharacter() { //Finds character in a string
-
+//Note to self -> const string& reads the original without making a copy and does not modify the original
+int find(const string& str, char c) { //Finds character in a string
+   for (int i = 0; i < str.size(); ++i) {
+      if (str[i] == c) {
+         return i; //Returning the index where we found out character
+      }
+   }
+   return -1; //Only runs if the if statement does not return; the character was not found
 }
 
-int findSubstring() { //Finds substring in a string
-
+int find(const string& str, const std:: string& substr) { //Finds a substring
+   //Add code here
 }
